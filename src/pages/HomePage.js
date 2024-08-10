e2a80cdcb5e621a2270d9e5bb97af4aa4c2975cd
Gus14939 <148546545+Gus14939@users.noreply.github.com>
2024-08-10 13:13:22 +1000

    feat: file clean up /. Home button to other pages

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import PlantOfTheDay from '../components/PlantOfTheDay';
-import HomeHeroIntro from '../components/HomeHeroIntro';
-import Listings from '../components/Listings';
-import SearchComponent from '../components/SearchComponent';
+import React from "react";
+import PlantOfTheDay from "../components/PlantOfTheDay";
+import HomeHeroIntro from "../components/HomeHeroIntro";
+import Listings from "../components/Listings";
+import SearchComponent from "../components/SearchComponent";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    return (
-        <div className='home'>
-            <HomeHeroIntro />
-            <PlantOfTheDay />
-            <div className='searchies'>
-                <SearchComponent />
-            </div>
-            <div className="homeListings">
-                <Listings />
-            </div>
+  return (
+    <div className="home">
+      <HomeHeroIntro />
+      <PlantOfTheDay />
+      <div className="homeInteractions">
+        <Link className="postYourPlant_homeBtn" to="/posts/postYourPlant">
+          <div>Post your plant</div>
+        </Link>
+        <div className="homePlantSearch">
+          <SearchComponent />
         </div>
-    );
-}
+      </div>
+      <div className="homeListings">
+        <Listings />
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
